c697842f45159f87e4b987f9be8695c1409c9003
Kevin Matlock <kevin.matlock@omicsautomation.com>
2024-10-20 19:00:41 -0700

    Added MappingProxy type.

--- src/sealed.rs
+++ src/sealed.rs
@@ -1,7 +1,5 @@
 use crate::types::{
-    PyBool, PyByteArray, PyBytes, PyCapsule, PyComplex, PyDict, PyFloat, PyFrozenSet, PyList,
-    PyMapping, PyModule, PySequence, PySet, PySlice, PyString, PyTraceback, PyTuple, PyType,
-    PyWeakref, PyWeakrefProxy, PyWeakrefReference,
+    PyBool, PyByteArray, PyBytes, PyCapsule, PyComplex, PyDict, PyFloat, PyFrozenSet, PyList, PyMapping, PyMappingProxy, PyModule, PySequence, PySet, PySlice, PyString, PyTraceback, PyTuple, PyType, PyWeakref, PyWeakrefProxy, PyWeakrefReference
 };
 use crate::{ffi, Bound, PyAny, PyResult};
 
@@ -33,6 +31,7 @@ impl Sealed for Bound<'_, PyFloat> {}
 impl Sealed for Bound<'_, PyFrozenSet> {}
 impl Sealed for Bound<'_, PyList> {}
 impl Sealed for Bound<'_, PyMapping> {}
+impl Sealed for Bound<'_, PyMappingProxy> {}
 impl Sealed for Bound<'_, PyModule> {}
 impl Sealed for Bound<'_, PySequence> {}
 impl Sealed for Bound<'_, PySet> {}
